783b3f8255b3a107a70f4fb390bbc16dee2bc372
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-12-06 17:04:32 -0800

    add basic form functionality

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Dictionary.css";
 
 const Dictionary = () => {
-	return <div className="Dictionary">Hello from Dictionary Component</div>;
+	const [keyword, setKeyword] = useState("");
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		setKeyword("");
+		alert(`Searching for ${keyword}`);
+	}
+
+	return (
+		<div className="Dictionary">
+			<form onSubmit={handleSubmit}>
+				<input
+					type="search"
+					onChange={(e) => setKeyword(e.target.value)}
+					value={keyword}
+				></input>
+			</form>
+		</div>
+	);
 };
 
 export default Dictionary;
